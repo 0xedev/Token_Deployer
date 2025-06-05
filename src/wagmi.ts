4c082fe93f8d09705cee24d701f288036991bb25
0xedev <oxdeve@gmail.com>
2025-06-05 16:01:57 +0100

    new

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,13 +1,15 @@
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 import { http, createConfig, injected } from "wagmi";
-import { base, mainnet } from "wagmi/chains";
+import { base, mainnet, celo, monadTestnet } from "wagmi/chains";
 
 export const config = createConfig({
-  chains: [base, mainnet],
+  chains: [base, mainnet, celo, monadTestnet],
   connectors: [farcasterFrame(), injected()],
   transports: {
     [base.id]: http(),
     [mainnet.id]: http(import.meta.env.VITE_RPC_URL),
+    [celo.id]: http(import.meta.env.VITE_CELO_RPC_URL),
+    [monadTestnet.id]: http(import.meta.env.VITE_MONAD_TESTNET_RPC_URL),
   },
 });
 
